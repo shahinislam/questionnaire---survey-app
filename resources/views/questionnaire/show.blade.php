c15544dad683b2dd2d3fc200c78657ae0385a2b2
Shahinur Islam <shahin.sec.cse@gmail.com>
2020-01-19 21:50:45 +0600

    first commit

---
+++ resources/views/questionnaire/show.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ $questionnaire->title }}</div>
+
+                <div class="card-body">
+                    <a href="/questionnaires/{{ $questionnaire->id }}/questions/create" class="btn btn-info">Add New Question</a>
+                    <a href="/surveys/{{ $questionnaire->id }}-{{ Str::slug($questionnaire->title) }}" class="btn btn-success">Take Survey</a>
+                </div>
+            </div>
+
+            @foreach($questionnaire->questions as $question)
+                <div class="card mt-4">
+                    <div class="card-header">{{ $question->question }}</div>
+
+                    <div class="card-body">
+                        <ul class="list-group">
+                            @foreach($question->answers as $answer)
+                                <li class="list-group-item d-flex justify-content-between">
+                                    <div>{{ $answer->answer }}</div>
+                                    @if($question->responses->count())
+                                        <div class="text-primary">{{ intval(($answer->responses->count()*100)/($question->responses->count())) }}%</div>
+                                    @endif
+                                </li>
+                            @endforeach
+                        </ul>
+                    </div>
+
+                    <div class="card-footer">
+                        <form action="/questionnaires/{{ $questionnaire->id }}/questions/{{ $question->id }}" method="post">
+                            @method('DELETE')
+                            @csrf
+
+                            <button class="btn btn-sm btn-outline-danger" type="submit">Delete Question</button>
+                        </form>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
+</div>
+@endsection
